26c4d24d94ac49b7a2f093f336723075ca64fbce
Yoorim Choi <penguin0807@snu.ac.kr>
2025-09-06 11:17:49 +0900

    Upload assignment0 skeleton

---
+++ assignment0/assignment0-skeleton/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    private lazy var emojiLabel: UILabel = {
+        let label = UILabel()
+        label.text = "🎉"
+        label.font = .systemFont(ofSize: 48)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO
+    }
+    
+    // TODO
+}
